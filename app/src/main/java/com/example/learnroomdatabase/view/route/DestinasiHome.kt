367bbf565d5090050cc645258f56feabe88906ff
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:22:44 +0700

    feat: Create DestinasiHome navigation destination
    Creates the `DestinasiHome` object, which implements the `DestinasiNavigasi` interface. This object defines the navigation route and title for the home screen.
    
    It sets the `route` to "home" and the `titleRes` to `R.string.app_name`.

--- app/src/main/java/com/example/learnroomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/learnroomdatabase/view/route/DestinasiHome.kt
@@ -2,3 +2,7 @@ package com.example.learnroomdatabase.view.route
 
 import com.example.learnroomdatabase.R
 
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override  val titleRes = R.string.app_name
+}
