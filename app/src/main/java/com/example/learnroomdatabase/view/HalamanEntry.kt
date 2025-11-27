109afefd6bb6b26c60b7af5f483d2c8514c6b6fc
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:26:34 +0700

    feat: Create initial structure for `HalamanEntry` composable
    Adds a new file, `HalamanEntry.kt`, which sets up the basic structure for a new screen.
    
    This initial implementation includes:
    *   Importing necessary Jetpack Compose UI components like `Column`, `Scaffold`, `OutlinedTextField`, and `Button`.
    *   Importing dependencies for ViewModel integration, navigation, and coroutines.

---
+++ app/src/main/java/com/example/learnroomdatabase/view/HalamanEntry.kt
@@ -0,0 +1,34 @@
+package com.example.learnroomdatabase.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.learnroomdatabase.R
+import com.example.learnroomdatabase.view.route.DestinasiEntry
+import com.example.learnroomdatabase.viewmodel.DetailSiswa
+import com.example.learnroomdatabase.viewmodel.EntryViewModel
+import com.example.learnroomdatabase.viewmodel.PenyediaViewModel
+import com.example.learnroomdatabase.viewmodel.UIStateSiswa
+import kotlinx.coroutines.launch
+
