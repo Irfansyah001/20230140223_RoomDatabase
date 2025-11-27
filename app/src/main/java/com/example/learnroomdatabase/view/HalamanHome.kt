7ae6265c63dc16f42eee50f56db3f2fa6002a490
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:28:39 +0700

    feat: Create Home screen UI scaffold
    Creates the initial `HalamanHome.kt` file, which sets up the basic UI structure for the home screen using a `Scaffold`.
    
    This initial implementation includes:
    *   A `TopAppBar` with the application title.
    *   A placeholder for the screen content.
    *   A `FloatingActionButton` with an "Add" icon.

---
+++ app/src/main/java/com/example/learnroomdatabase/view/HalamanHome.kt
@@ -0,0 +1,25 @@
+package com.example.learnroomdatabase.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.learnroomdatabase.R
+import com.example.learnroomdatabase.room.Siswa
+import com.example.learnroomdatabase.viewmodel.HomeViewModel
+import com.example.learnroomdatabase.viewmodel.PenyediaViewModel
+
