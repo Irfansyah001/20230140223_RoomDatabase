73662fc5c85a7b042dd05638ebb38ba5bdba8c5e
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:25:15 +0700

    feat: Implement navigation host with composable routes
    Creates the `HostNavigasi` composable function, which sets up the `NavHost` for the application's navigation graph.
    
    This implementation includes:
    *   Setting the `startDestination` to `DestinasiHome.route`.
    *   Defining a composable route for the `HomeScreen`, which handles navigation to the item entry screen.
    *   Defining a composable route for the `EntrySiswaScreen`, which handles the action to navigate back.

--- app/src/main/java/com/example/learnroomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/learnroomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -20,3 +20,24 @@ fun SiswaApp(
     HostNavigasi(navController = navController, modifier = modifier)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
