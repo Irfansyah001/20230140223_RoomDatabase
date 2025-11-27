9a237e12d601e5c269132e0cd6c6e4050898009a
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:24:12 +0700

    feat: Set up basic navigation host with NavHost
    Creates a new composable function `PetaNavigasi` to manage the application's navigation graph using `NavHost`.
    
    This initial setup defines two composable routes:
    *   `DestinasiHome.route`: Navigates to the `HomeScreen`.
    *   `DestinasiEntry.route`: Navigates to the `EntrySiswaScreen`.

---
+++ app/src/main/java/com/example/learnroomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.learnroomdatabase.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.learnroomdatabase.view.EntrySiswaScreen
+import com.example.learnroomdatabase.view.HomeScreen
+import com.example.learnroomdatabase.view.route.DestinasiEntry
+import com.example.learnroomdatabase.view.route.DestinasiHome
+
