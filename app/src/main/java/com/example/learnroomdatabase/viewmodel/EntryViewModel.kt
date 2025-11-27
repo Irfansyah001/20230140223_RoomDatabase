bb9d94d71f4ff35b23de7187c79f48f4a9876763
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:31:12 +0700

    feat: Add initial structure for EntryViewModel
    Creates the `EntryViewModel` class, which extends `ViewModel`. This initial setup includes necessary imports from `androidx.compose.runtime` and `androidx.lifecycle`, as well as project-specific imports for `RepositoriSiswa` and the `Siswa` entity.

---
+++ app/src/main/java/com/example/learnroomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.learnroomdatabase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.learnroomdatabase.repositori.RepositoriSiswa
+import com.example.learnroomdatabase.room.Siswa
+
