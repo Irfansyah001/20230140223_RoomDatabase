7f15fe644c7551726c9df9974a804a36c88e761c
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 17:22:09 +0700

    feat: Define navigation destination for entry screen
    Creates the `DestinasiEntry` object, which implements the `DestinasiNavigasi` interface.
    
    This object defines the navigation route and title for the student entry screen:
    *   `route`: "item_entry"
    *   `titleRes`: `R.string.entry_siswa`

--- app/src/main/java/com/example/learnroomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/learnroomdatabase/view/route/DestinasiEntry.kt
@@ -2,3 +2,7 @@ package com.example.learnroomdatabase.view.route
 
 import com.example.learnroomdatabase.R
 
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
