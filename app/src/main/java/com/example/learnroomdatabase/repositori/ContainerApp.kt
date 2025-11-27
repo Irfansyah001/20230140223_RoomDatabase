f9b42fe169f32a539f97024b3ad6a095225686eb
Irfansyah001 <syahirfan545@gmail.com>
2025-11-27 12:36:15 +0700

    feat: Implement dependency injection container
    Creates a dependency injection (DI) container to manage and provide dependencies throughout the application.
    
    This includes:
    *   An `AplikasiSiswa` class that extends `Application` to hold the application-wide container instance.
    *   A `ContainerApp` interface defining the contract for the container, specifying access to `RepositoriSiswa`.
    *   A `ContainerDataApp` class that implements the `ContainerApp` interface. It initializes and provides an `OfflineRepositoriSiswa` instance using the DAO from the singleton `DatabaseSiswa`.

---
+++ app/src/main/java/com/example/learnroomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,32 @@
+package com.example.learnroomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.learnroomdatabase.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
+
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance used by the rest of classes to obtain dependencies
+     * AppContainer instance digunakan oleh kelas lainnya untuk mendapatkan dependensi
+     */
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
